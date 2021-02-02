457f9de7a5e62ac1fc7b90e62131261454e4f59a
Mayuresh Sardesai <sardesaimayuresh@gmail.com>
2021-02-02 08:45:16 -0500

    updated read bundler to visualize cam poses

--- 00_ScriptsForAnalysisOfClusters/ReadBundleOut.py
+++ 00_ScriptsForAnalysisOfClusters/ReadBundleOut.py
@@ -1,16 +1,10 @@
 import numpy as np 
+import pandas as pd
 from collections import defaultdict
 # import json, codecs
 import os
 import matplotlib.pyplot as plt
-
-root_dir='D:/00_NCSU/00_Resources/00_Datasets/PartTimePSA/ClusteringExp/20'
-bundle_files=[]
-for root, dirs, files in os.walk(root_dir):
-    for file in files:
-        if file.endswith("020.out"):
-             print(os.path.join(root, file))
-             bundle_files.append(os.path.join(root, file))
+from pyntcloud import PyntCloud
 
 def read_bundle_out(bundle_file):
     # fp = open(root+'bundle_020.out', "r")
@@ -84,6 +78,18 @@ def read_bundle_out(bundle_file):
         
     # with open('point-data-2-20.json', 'w') as fp: 
     #     json.dump(points, fp)
+    
+def reject_outliers_2d(data, m=2):
+    col=data[2,:]
+    return data[:,abs(col - np.mean(col)) < m * np.std(col)]
+
+root_dir='D:/00_NCSU/00_Resources/00_Datasets/PartTimePSA/ClusteringExp/20'
+bundle_files=[]
+for root, dirs, files in os.walk(root_dir):
+    for file in files:
+        if file.endswith("020.out"):
+             print(os.path.join(root, file))
+             bundle_files.append(os.path.join(root, file))
 
 cam_poses=[]
 pt_cld_clusters=[]
@@ -94,14 +100,57 @@ for file in bundle_files:
 
 minimums=[]
 maximums=[]
-for pt_cld in pt_cld_clusters:
+modes=[]
+total_cld=np.array(pt_cld_clusters[0]['pos'])
+total_cld_col=np.array(pt_cld_clusters[0]['col'])
+for i,pt_cld in enumerate(pt_cld_clusters):
     points = np.array(pt_cld['pos'])
+    # points_filt = reject_outliers_2d(points)
+    points_filt=points
+    col = np.array(pt_cld['col'])
+    # col_filt = reject_outliers_2d(col)
+    col_filt=col
     # print(points.shape)
-    print(np.amin(points, axis=0))
-    minimums.append(np.amin(points, axis=0))
-    maximums.append(np.amax(points, axis=0))
+    # print(np.amin(points_filt, axis=0))
+    minimums.append(np.amin(points_filt, axis=0))
+    modes.append(plt.hist(total_cld[:,2])[1][np.argmax(plt.hist(total_cld[:,2])[0])])
+    maximums.append(np.amax(points_filt, axis=0))
+    # print(f'Volume and XY area of bounding box for cluster {i}', \
+    #   (maximums[-1][0]-minimums[-1][0])* (maximums[-1][1]-minimums[-1][1])\
+    #       *(maximums[-1][2]-minimums[-1][2]),((maximums[-1][0]-minimums[-1][0])* \
+    #       (maximums[-1][1]-minimums[-1][1])*(maximums[-1][2]-minimums[-1][2]))\
+    #       /(maximums[-1][2]-minimums[-1][2]))
+        
+    
+    if i>=1:
+    #     # print([minimums[0][2]])
+    #     print('min before offset')
+    #     print(np.amin(points_filt, axis=0))
+    #     # points_filt=points_filt-np.array([0,0,minimums[0][2]])
+    #     points_filt=points_filt-np.array([0,0,modes[0]])
+    #     print('min after offset')
+    #     print(np.amin(points_filt, axis=0))
+        total_cld=np.vstack((total_cld, points))
+        total_cld_col=np.vstack((total_cld_col, col))
 minimums=np.array(minimums)
+modes=np.array(modes)
 maximums=np.array(maximums)
+
+ct=0
+cam_centers=[[] for i in range(len(cam_poses))]
+cam_look_vectors=[[] for i in range(len(cam_poses))]
+for i,pose in enumerate(cam_poses): 
+    for j in range(len(pose['R'])):
+        c=-np.dot(np.array(pose['R'][j]).T,pose['t'][j])
+        ct+=1
+        # print(ct,c,end='\n')
+        cam_centers[i].append(c)
+        cam_look_vectors[i].append(np.dot(np.array(pose['R'][j]).T,\
+                                       np.array([0,0,1])))
+
+cam_centers=np.array(cam_centers)
+cam_look_vectors=np.array(cam_look_vectors)
+
 
 plt.figure()
 cls_nums=np.linspace(1,len(bundle_files),len(bundle_files))
@@ -115,5 +164,43 @@ cls_nums=np.linspace(1,len(bundle_files),len(bundle_files))
 plt.plot(cls_nums, maximums[:,0], label='maxx')
 plt.plot(cls_nums, maximums[:,1], label='maxy')
 plt.plot(cls_nums, maximums[:,2], label='maxz')
-plt.legend()
+
+plt.plot(cls_nums, modes[:], label='modez')
+# plt.plot(cls_nums, modes[:,1], label='modey')
+# plt.plot(cls_nums, modes[:,2], label='modez')
+plt.legend(loc=3, prop={'size':6})
+plt.title('Variation in x,y,z bounds according to cluster')
 plt.show()
+
+plt.figure()
+plt.plot(cam_centers[:,:,2].reshape(-1,1), label='z coord')
+plt.xlabel('frame number')
+plt.ylabel('camera z coordinate')
+plt.title('variation in camera pose (z coordinate) according to frame')
+plt.legend(loc=3, prop={'size':6})
+plt.show()
+
+center_data=cam_centers.reshape(cam_centers.shape[0]*cam_centers.shape[1],\
+                                cam_centers.shape[2])
+
+df=pd.DataFrame(
+    # same arguments that you are passing to visualize_pcl
+    data=np.vstack((np.hstack((total_cld, total_cld_col)),\
+                   np.hstack((center_data,np.ones(np.shape(center_data))*255)))),
+    columns=["x", "y", "z", "red", "green", "blue"])
+df[['red','green','blue']] = df[['red','green','blue']].astype(np.uint8)
+cloud = PyntCloud(df)
+
+
+
+
+
+cloud.to_file("offset_cld.ply")
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+xdata = center_data[:,0]
+ydata = center_data[:,1]
+zdata = center_data[:,2]
+ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens');
+ax.scatter3D(points[:,0], points[:,1], points[:,2], c=points[:,2], cmap='Reds');
--- 00_ScriptsForAnalysisOfClusters/ReadPMVS.py
+++ 00_ScriptsForAnalysisOfClusters/ReadPMVS.py
@@ -6,11 +6,12 @@ from collections import defaultdict
 # import json, codecs
 import os
 import matplotlib.pyplot as plt
-from pyntcloud import PyntCloud
+# from pyntcloud import PyntCloud
 # from ReadBundleOut import read_bundle_out
 
-root_dir='D:/00_NCSU/00_Resources/00_Datasets/PartTimePSA/ClusteringExp/40'
-
+root_dir='D:/00_NCSU/00_Resources/00_Datasets/PartTimePSA/ClusteringExp/MD-301/20'
+cld_dir='D:/00_NCSU/Spring2021/00_PartTime/00_Scripts/'
+ply_dir=cld_dir+'MD-301-PLY/'
 def read_point_clouds_from_folder(root_dir, voxel_size=0.0):
     pmvs_files=[]
     pcds=[]
@@ -34,9 +35,9 @@ def scale_pt_clouds(pcds):
         obb.color = (0, 1, 0)
         volumes.append(obb.volume())
     print(volumes)
-    max_volume=max(volumes)
+    mean_volume=np.mean(volumes)
     for i,pcd in enumerate(pcds):
-        pcd_scaled=pcd.scale(max_volume/volumes[i], center=pcd.get_center())
+        pcd_scaled=pcd.scale(mean_volume/volumes[i], center=pcd.get_center())
         pcds_scaled.append(pcd_scaled)
         # if i>1:
         #     pcd_down.scale(volumes[-2]/volumes[-1], center=pcd_down.get_center())
@@ -48,7 +49,20 @@ def scale_pt_clouds(pcds):
         #     print(volumes)
     return pcds_scaled
 
-voxel_size = 0.02
+def display_inlier_outlier(cloud, ind):
+    inlier_cloud = cloud.select_by_index(ind)
+    outlier_cloud = cloud.select_by_index(ind, invert=True)
+
+    print("Showing outliers (red) and inliers (gray): ")
+    outlier_cloud.paint_uniform_color([1, 0, 0])
+    inlier_cloud.paint_uniform_color([0.8, 0.8, 0.8])
+    o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud],
+                                      zoom=0.3412,
+                                      front=[0.4257, -0.2125, -0.8795],
+                                      lookat=[2.6172, 2.0475, 1.532],
+                                      up=[-0.0694, -0.9768, 0.2024])
+
+voxel_size = 0.05
 pcds_down = read_point_clouds_from_folder(root_dir,voxel_size)
 pcds_scaled=scale_pt_clouds(pcds_down)
 o3d.visualization.draw_geometries(pcds_scaled,
@@ -71,7 +85,6 @@ o3d.visualization.draw_geometries(pcds_scaled,
                                       # front=[0.5439, -0.2333, -0.8060],
                                       # lookat=[2.4615, 2.1331, 1.338],
                                       # up=[-0.1781, -0.9708, 0.1608])
-                 
 
 def pairwise_registration(source, target):
     print("Apply point-to-plane ICP")
@@ -119,10 +132,10 @@ def full_registration(pcds, max_correspondence_distance_coarse,
     return pose_graph
 
 print("Full registration ...")
-max_correspondence_distance_coarse = voxel_size * 15
+max_correspondence_distance_coarse = voxel_size * 25
 max_correspondence_distance_fine = voxel_size * 1.5
 with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
-    pose_graph = full_registration(pcds_down,
+    pose_graph = full_registration(pcds_scaled,
                                    max_correspondence_distance_coarse,
                                    max_correspondence_distance_fine)
     
@@ -140,14 +153,34 @@ with o3d.utility.VerbosityContextManager(
         option)
     
 print("Transform points and display")
-for point_id in range(len(pcds_down)):
+for point_id in range(len(pcds_scaled)):
     print(pose_graph.nodes[point_id].pose)
-    pcds_down[point_id].transform(pose_graph.nodes[point_id].pose)
-o3d.visualization.draw_geometries(pcds_down,
+    pcds_scaled[point_id].transform(pose_graph.nodes[point_id].pose)
+o3d.visualization.draw_geometries(pcds_scaled,
                                   zoom=0.3412,
                                   front=[0.4257, -0.2125, -0.8795],
                                   lookat=[2.6172, 2.0475, 1.532],
                                   up=[-0.0694, -0.9768, 0.2024])
+
+
+pcd_combined = o3d.geometry.PointCloud()
+for point_id in range(len(pcds_scaled)):
+    # pcds_scaled[point_id].transform(pose_graph.nodes[point_id].pose)
+    o3d.io.write_point_cloud(ply_dir+str(point_id)+".ply", pcds_scaled[point_id])
+    pcd_combined += pcds_scaled[point_id]
+pcd_combined_down = pcd_combined.voxel_down_sample(voxel_size=voxel_size)
+o3d.io.write_point_cloud(ply_dir+"multiway_registration.ply", pcd_combined_down)
+o3d.visualization.draw_geometries([pcd_combined_down],
+                                      zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
+
+print("Statistical oulier removal")
+cl, ind = pcd_combined_down.remove_statistical_outlier(nb_neighbors=20,
+                                                    std_ratio=2.0)
+display_inlier_outlier(pcd_combined_down, ind)
+o3d.io.write_point_cloud(ply_dir+"multiway_registration_no_outlier.ply", cl)
 # aabb = pcd.get_axis_aligned_bounding_box()
 # aabb.color = (1, 0, 0)
 # obb = pcd.get_oriented_bounding_box()
