a1c8c31870467396affeeacde0d4a4277aa9cc7d
Shilpa Kancharla <shilpakancharla18@gmail.com>
2021-06-09 13:47:38 -0400

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,26 @@
+# General
+RESOURCE_GROUP = '3DWeeds'
+STORAGE_ACCOUNT = 'weedsmedia'
+CONTAINER_NAME = 'weeds3d'
+
+# Credientials (App Registrations)
+SUBSCRIPTION_ID = '576953e3-6b3c-49d6-8187-c020e121d81d'
+CLIENT_ID = 'd3081302-1176-4f12-a1be-d984b1938351'
+TENANT_ID = 'de53dfc3-80e5-4579-a0bd-0f28aea1a22d'
+SECRET_VALUE = 'B.-Q0-93.o5lZq6cOtc-rYq0T_N9MmF_3f'
+SECRET_ID = 'B.-Q0-93.o5lZq6cOtc-rYq0T_N9MmF_3f'
+VAULT_URL = 'https://paulamachine9999kv.vault.usgovcloudapi.net/'
+
+# Storage account
+KEY_1 = 'lgkp0IaGalyNoguw/lVtyYPpizhTyaVebm+gA8g9G463DBwqjf8Jj51e3sqUTHzKqQBlc5BXPYS0t24F7KbUmA=='
+CONNECTION_STRING_1 = 'DefaultEndpointsProtocol=https;AccountName=weedsmedia;AccountKey=lgkp0IaGalyNoguw/lVtyYPpizhTyaVebm+gA8g9G463DBwqjf8Jj51e3sqUTHzKqQBlc5BXPYS0t24F7KbUmA==;EndpointSuffix=core.usgovcloudapi.net'
+KEY_2 = '5N430dfSkpEmkvC0n62Qo9qdic97DeY93tSqz5gVm77Mef4Zpz31PoP6q5hCdQieAVC8gCVnbYcQuTdq9zolzA=='
+CONNECTION_STRING_2 = 'DefaultEndpointsProtocol=https;AccountName=weedsmedia;AccountKey=5N430dfSkpEmkvC0n62Qo9qdic97DeY93tSqz5gVm77Mef4Zpz31PoP6q5hCdQieAVC8gCVnbYcQuTdq9zolzA==;EndpointSuffix=core.usgovcloudapi.net'
+
+# Virtual machine
+VIRTUAL_MACHINE_NAME = '3Dimaging'
+PUBLIC_IP_ADDRESS = '23.97.11.133'
+LOCATION = 'USGov Virginia'
+SSH_KEY = 'ssh -i id_weeds3D azureuser@23.97.11.133'
+PASSKEY = 'ConnectedF@rms2020'
+OBJECT_ID = 'c635e0f3-d7b3-4b5b-8711-47256e8dbbf5'
