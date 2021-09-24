b30c265e6f05a866ef9bfd85619e3a495e3e25c3
mkutu <54868229+mkutu@users.noreply.github.com>
2021-09-24 17:17:33 -0500

    added focal length variable

--- GoPro_calibration/calculate_fl.py
+++ GoPro_calibration/calculate_fl.py
@@ -9,6 +9,7 @@ lst = npz_calib_file.files
 
 distCoeff = npz_calib_file['distCoeff']
 intrinsic_matrix = npz_calib_file['intrinsic_matrix']
+focalLength = npz_calib_file["focalLength"]
 
 for item in lst:
 	print(item)
@@ -20,6 +21,6 @@ print('Finished loading files')
 print(' ')
 print('Starting to undistort the video....')
 
-#print('fovx, fovy, focalLength, principalPoint, aspectRatio ', cv2.calibrationMatrixValues(intrinsic_matrix, (3840, 2160), 6.17, 4.63))
+print('fovx, fovy, focalLength, principalPoint, aspectRatio ', cv2.calibrationMatrixValues(intrinsic_matrix, (3840, 2160), 6.17, 4.63))
 
-print('fovx, fovy, focalLength, principalPoint, aspectRatio ', cv2.calibrationMatrixValues(intrinsic_matrix, (1920, 1080), 6.17, 4.63)) #Change the image size (1920,1080)
+# print('fovx, fovy, focalLength, principalPoint, aspectRatio ', cv2.calibrationMatrixValues(intrinsic_matrix, (1920, 1080), 6.17, 4.63)) #Change the image size (1920,1080)
